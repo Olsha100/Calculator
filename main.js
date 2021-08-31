1cf917785a96a12d2b1ee096f5742ef4241903fc
Olsha100 <70657477+Olsha100@users.noreply.github.com>
2021-08-31 23:39:48 +0200

    Equation validation using regular expressions

--- main.js
+++ main.js
@@ -1,34 +1,37 @@
-let cth = document.querySelectorAll(".catch");
-let para = document.querySelector(".score");
+let btn = document.querySelectorAll(
+  '.container__button:not(.container__button--sumup)'
+);
+let score = document.querySelector('.container__score');
 
-
-for (let i=0;i<cth.length;i++) {
-    cth[i].addEventListener('click',addToEquation,false);
-};
-
-function addToEquation(e){
-    if(!para.textContent && (e.target.textContent === '*' || 
-                             e.target.textContent === '/' || 
-                             e.target.textContent === '+' || 
-                             e.target.textContent === "0" ||
-                             e.target.textContent === "." ||
-                             e.target.textContent === "00")){
-        para.textContent = null;
-    }
-    else if(para.textContent.includes(".") && e.target.textContent === "."){}
-    else{para.textContent += e.target.textContent;}
-    
+for (let i = 0; i < btn.length; i++) {
+  btn[i].addEventListener('click', addToEquation, false);
 }
 
-let cancel = document.querySelector("#C");
-cancel.addEventListener("click",() => {
-    para.textContent = null;
-},false)
+function addToEquation(e) {
+  if (
+    //Checking if any of the following characters tries to be typed in the beggining of the equation
+    (!score.textContent && /[*\/+0.00=-]/.test(e.target.textContent)) ||
+    //There can't be more than one decimal operator in a number
+    (score.textContent.includes('.') && e.target.textContent === '.') ||
+    //There can't be two operators one by one in an equation
+    (/([*\/+0.00=-])(?!\S)/.test(score.textContent) &&
+      /([*\/+0.00=-])(?!\S)/.test(e.target.textContent))
+  ) {
+  } else {
+    score.textContent += e.target.textContent;
+  }
+}
 
+let cancel = document.querySelector('#C');
+cancel.addEventListener(
+  'click',
+  () => {
+    score.textContent = null;
+  },
+  false
+);
 
-let equation = document.querySelector("#equation");
-equation.addEventListener("click",()=>{
-   
-   para.textContent = eval(para.textContent);
-    
-})
+let equation = document.querySelector('#equation');
+equation.addEventListener('click', () => {
+  score.textContent = eval(score.textContent);
+});
