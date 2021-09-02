3b1265dac7b73d25e0be6f259f5aefe4ecc6622f
Olsha100 <70657477+Olsha100@users.noreply.github.com>
2021-09-02 20:42:44 +0200

    Added keyboard support

--- main.js
+++ main.js
@@ -3,35 +3,66 @@ let btn = document.querySelectorAll(
 );
 let score = document.querySelector('.container__score');
 
-for (let i = 0; i < btn.length; i++) {
-  btn[i].addEventListener('click', addToEquation, false);
-}
+btn.forEach((e) => e.addEventListener('click', addToEquationClick));
 
-function addToEquation(e) {
+document.addEventListener('keydown', addToEquationKeyDown);
+
+function addToEquationKeyDown(e) {
   if (
     //Checking if any of the following characters tries to be typed in the beggining of the equation
-    (!score.textContent && /[*\/+0.00=-]/.test(e.target.textContent)) ||
+    (!score.textContent && /[*\/+0.=-]/.test(e.key)) ||
+    //There can't be more than one decimal operator in a number
+    (score.textContent.includes('.') && e.key === '.') ||
+    //There can't be two operators one by one in an equation
+    (/([*\/+0.=-])(?!\S)/.test(score.textContent) &&
+      /([*\/+0.=-])(?!\S)/.test(e.key))
+  ) {
+  } else if (/[0-9+\-*\/=]/.test(e.key)) {
+    score.textContent += e.key;
+  }
+}
+
+function addToEquationClick(e) {
+  if (
+    //Checking if any of the following characters tries to be typed in the beggining of the equation
+    (!score.textContent && /[*\/+0.=-]/.test(e.target.textContent)) ||
     //There can't be more than one decimal operator in a number
     (score.textContent.includes('.') && e.target.textContent === '.') ||
     //There can't be two operators one by one in an equation
-    (/([*\/+0.00=-])(?!\S)/.test(score.textContent) &&
-      /([*\/+0.00=-])(?!\S)/.test(e.target.textContent))
+    (/([*\/+0.=-])(?!\S)/.test(score.textContent) &&
+      /([*\/+0.=-])(?!\S)/.test(e.target.textContent))
   ) {
   } else {
     score.textContent += e.target.textContent;
   }
 }
 
+//Cancelling
+
 let cancel = document.querySelector('#C');
-cancel.addEventListener(
-  'click',
-  () => {
-    score.textContent = null;
-  },
-  false
-);
+
+cancel.addEventListener('click', cancelEquation);
+document.addEventListener('keydown', (e) => {
+  if (['c', 'C', 'Escape'].includes(e.key)) {
+    cancelEquation();
+  }
+});
+
+function cancelEquation() {
+  score.textContent = null;
+}
+
+//Solving equation
 
 let equation = document.querySelector('#equation');
-equation.addEventListener('click', () => {
-  score.textContent = eval(score.textContent);
+
+equation.addEventListener('click', solveEquation);
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter') {
+    solveEquation();
+  }
 });
+
+function solveEquation() {
+  score.textContent = eval(score.textContent);
+}
